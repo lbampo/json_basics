725a707d0598a58d6a8e45ea304038b6d0cf98f4
Lennox Bampoe-Addo <LBampoe-Addo@spartaglobal.com>
2019-10-03 11:23:52 +0100

    Added to file

---
+++ json_loads.py
@@ -0,0 +1,14 @@
+import json
+
+with open('new_json_file.json', 'r') as jsonfile:
+    car_dictionary = json.load(jsonfile)
+
+    print(car_dictionary)
+    print(type(car_dictionary))
+    print(type(car_dictionary))
+
+
+
+
+
+
